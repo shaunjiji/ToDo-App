a8c2ba2f3bebc55e4e33f540eb203db7eee659b4
Shaun Jiji <shaunjiji123@gmail.com>
2023-05-25 10:05:35 -0400

    add images in homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Home from "./pages/home/Home";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <main className="main-container">
+      <Home />
+    </main>
+  );
 }
 
 export default App;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,32 @@
 import React from "react";
 import "./home.css";
-import {ReactComponent as Moon} from "../../assets/icon-moon.svg"
-import {ReactComponent as Sun} from "../../assets/icon-sun.svg"
-
+import { ReactComponent as Moon } from "../../assets/icon-moon.svg";
+import { ReactComponent as Sun } from "../../assets/icon-sun.svg";
 
 const Home = () => {
   return (
     <section className="home-page-container">
-        <img alt="homepage-light"className="home-page-image" id="light-bg" src="images/bg-desktop-light.jpg"/>
-    </section>
-  )
-}
+      <img
+        className="home-page-image"
+        id="light-bg"
+        src="images/bg-desktop-light.jpg"
+        alt="homepage-light"
+      />
+      <img
+        className="home-page-image hidden"
+        id="dark-bg"
+        src="images/bg-desktop-dark.jpg"
+        alt="homepage-dark"
+      />
 
-export default Home
+      <div className="todo-container">
+        <div className="todo-wrapper">
+          <h1 className="title">todo</h1>
+          <div className="theme-icon"></div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
